89de2e409069c13df2548b632953ab366519b182
RileyBall3 <62024603+RileyBall3@users.noreply.github.com>
2022-12-01 11:44:30 -0500

    Update Counter.test.js

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,12 +1,17 @@
 // import necessary react testing library helpers here
+import { render, screen } from '@testing-library/react';
 // import the Counter component here
+import Counter from "../components/Counter";
 
 beforeEach(() => {
   // Render the Counter component here
+  render(<Counter />);
 })
 
 test('renders counter message', () => {
   // Complete the unit test below based on the objective in the line above
+  const counterMessage = screen.getByText(/counter/i);
+  expect(counterMessage).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
